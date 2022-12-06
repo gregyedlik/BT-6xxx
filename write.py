efbb496e1fe211b0738f74433495a6acbb865381
gregyedlik <89386294+gregyedlik@users.noreply.github.com>
2022-12-06 13:19:33 +0100

    Added turn-off options

--- write.py
+++ write.py
@@ -2,7 +2,7 @@ import serial
 import time
 import pickle
 
-ser = serial.Serial("/dev/tty.usbserial-A50285BI", baudrate=9600, parity=serial.PARITY_NONE,
+ser = serial.Serial("/dev/ttyS0", baudrate=9600, parity=serial.PARITY_NONE,
                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0)
 
 file = open('startup_pickle', 'rb')
@@ -26,6 +26,7 @@ def listen():
     time_spent = time.time() - beginning
     rx = ser.readline()
     print('RX\t' + f'{time_spent:.2f}' + '\t' + str(rx))
+    return rx
 
 
 for batch in startup:
@@ -40,10 +41,20 @@ print('Startup done')
 
 while 1:
     start = time.time() - startup[-4][0]['time']
+    responses = []
     for batch in startup[-4:]:
         for rec in batch:
             while time.time() - start < rec['time']:
                 time.sleep(0.001)
             ser.write(rec['byte'])
         print_batch(batch)
-        listen()
+        responses.append(listen())
+    # Switch off if unplugged
+    if len(responses) >= 4:
+        if responses[0] == responses[1] == responses[2] == responses[3] == b'':
+            print('Finished because seems unplugged.')
+            break
+    # Do not run over 2 hours
+    if time.time() - beginning > 2 * 60 * 60:
+        print('Finished after 2 hours.')
+        break
